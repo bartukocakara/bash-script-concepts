fc3e5908b432b64b76aeef67c846494ce3cb32a2
bartukocakara <bartu.kocakara@tardigrad.com.tr>
2024-11-11 17:45:28 +0300

    bash array init

--- bash-array.sh
+++ bash-array.sh
@@ -1,5 +1,17 @@
 #!/bin/bash
 
-echo "İsminiz : "
-read -a ISIMLER_DIZISI
-echo -e "Girilen isimler : ${ISIMLER_DIZISI[0]}, ${ISIMLER_DIZISI[1]}, ${ISIMLER_DIZISI[2]} "
+OS=( 'Linux' 'Windows', 'Unix' )
+
+echo "${OS[@]}" # Show all array elements
+echo "${OS[2]}" # Shows the 3rd element
+echo "${!OS[@]}" # Show indexes of all array
+echo "${#OS[@]}" # Show all elements number
+
+OS[3]='MAC'
+
+echo "${OS[@]}" # Lets check all array elements, if its added or not
+
+unset OS[1] # Remove second element of array
+echo "${OS[@]}" # Lets check all array elements, if its deleted or not
+
+echo "${!OS[@]}"
